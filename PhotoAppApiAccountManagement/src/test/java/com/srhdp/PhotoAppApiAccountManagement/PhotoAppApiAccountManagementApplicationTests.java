e7b70db47f5b5199c66215f1cc2db4cf1f50252b
Sout Rahim <sout.rahim@gmail.com>
2023-08-11 17:47:31 +0700

    init account management service

---
+++ PhotoAppApiAccountManagement/src/test/java/com/srhdp/PhotoAppApiAccountManagement/PhotoAppApiAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srhdp.PhotoAppApiAccountManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiAccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
