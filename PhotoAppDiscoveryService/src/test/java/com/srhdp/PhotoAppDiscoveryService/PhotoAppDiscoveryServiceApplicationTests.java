86829d8f350e868c0a863418d6da86776d9fe73b
Sout Rahim <sout.rahim@gmail.com>
2023-08-11 17:29:35 +0700

    Init photoappiscoveryservice

---
+++ PhotoAppDiscoveryService/src/test/java/com/srhdp/PhotoAppDiscoveryService/PhotoAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srhdp.PhotoAppDiscoveryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
