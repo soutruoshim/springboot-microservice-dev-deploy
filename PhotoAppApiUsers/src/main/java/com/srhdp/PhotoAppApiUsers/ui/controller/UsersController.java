d76b539ed58418e13403d01035441a824c38dcb7
Sout Rahim <sout.rahim@gmail.com>
2023-08-22 09:48:32 +0700

    set auto config from github using amqp

--- PhotoAppApiUsers/src/main/java/com/srhdp/PhotoAppApiUsers/ui/controller/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/srhdp/PhotoAppApiUsers/ui/controller/UsersController.java
@@ -7,6 +7,7 @@ import com.srhdp.PhotoAppApiUsers.ui.model.CreateUserResponseModel;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,12 +16,13 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/users")
 public class UsersController {
-
+    @Autowired
+    private Environment env;
     @Autowired
     UsersService usersService;
     @GetMapping("/status/check")
     public String status(){
-        return "Working";
+        return "Working on port " + env.getProperty("local.server.port") + ", with token = " + env.getProperty ("token.secret");
     }
 
     @PostMapping
