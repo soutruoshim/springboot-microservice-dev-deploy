1a30de297bbfce42ea348b05df84acb3b33d093a
Sout Rahim <sout.rahim@gmail.com>
2023-08-30 07:35:21 +0700

    add profile production and dev

--- PhotoAppApiUsers/src/main/java/com/srhdp/PhotoAppApiUsers/PhotoAppApiUsersApplication.java
+++ PhotoAppApiUsers/src/main/java/com/srhdp/PhotoAppApiUsers/PhotoAppApiUsersApplication.java
@@ -2,12 +2,15 @@ package com.srhdp.PhotoAppApiUsers;
 
 import com.srhdp.PhotoAppApiUsers.shared.FeignErrorDecoder;
 import feign.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,6 +18,9 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @EnableFeignClients
 public class PhotoAppApiUsersApplication {
+
+	@Autowired
+	Environment environment;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhotoAppApiUsersApplication.class, args);
@@ -35,15 +41,45 @@ public class PhotoAppApiUsersApplication {
 //	}
 
 	@Bean
+	@Profile("!production")
 	Logger.Level feignLoggerLevel()
 	{
 		return Logger.Level.FULL;
 	}
+	@Bean
+	@Profile("production")
+	Logger.Level feignProductionLoggerLevel(){
+		return Logger.Level.NONE;
+    }
 
 	@Bean
 	public FeignErrorDecoder getFeignErrorDecoder()
 	{
 		return new FeignErrorDecoder();
+	}
+
+
+	@Bean
+	@Profile("production")
+	public String createProductionBean(){
+		System.out.print("Production Bean =" + environment.getProperty("myapplication.environment") );
+		return "Production bean";
+	}
+
+
+	@Bean
+	@Profile("!production")
+	public String createNotProductionBean(){
+		System.out.print("Not Production Bean = " + environment.getProperty("myapplication.environment"));
+		return "Not Production bean";
+	}
+
+
+	@Bean
+	@Profile("default")
+	public String createDevelopmentBean(){
+		System.out.print("Development Bean = " + environment.getProperty("myapplication.environment"));
+		return "Development bean";
 	}
 
 }
